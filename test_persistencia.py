998f113daf56ff2adc503a1ee7a7187da9316d34
Alander Gimenez <82914806+alandergimenez@users.noreply.github.com>
2023-01-06 20:26:13 +0100

    eliminando errores fase 5

--- test_persistencia.py
+++ test_persistencia.py
@@ -1,15 +1,15 @@
 """ Pruebas Persistencia """
 
-import persistencia
 import pytest
+import persistencia
 
 def test_guardar_pedido():
     """ Prueba general """
-    with open("pedidos.txt", "w+", encoding="utf-8") as f:
+    with open("pedidos.txt", "w+", encoding="utf-8") as file_2:
         persistencia.guardar_pedido("Pedro", "Gil de Diego")
         persistencia.guardar_pedido("Michael", "Jordan")
-        firstline = f.readline()
-        secondline = f.readline()
-        f.close()
+        firstline = file_2.readline()
+        secondline = file_2.readline()
+        file_2.close()
     assert firstline == "-Pedro Gil de Diego\n"
     assert secondline == "-Michael Jordan\n"
