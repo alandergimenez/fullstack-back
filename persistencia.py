29c59f59cf54fb219c4356e73ee95bed75e38408
Alander Gimenez <82914806+alandergimenez@users.noreply.github.com>
2023-01-06 20:52:48 +0100

    eliminando errores fase 10

--- persistencia.py
+++ persistencia.py
@@ -1,11 +1,11 @@
 """ Guarda pedidos en archivo txt """
 
-with open("pedidos.txt", "w", encoding="utf-8") as file:
-    file.write("")
-    file.close()
+with open("pedidos.txt", "w", encoding="utf-8") as file_2:
+    file_2.write("")
+    file_2.close()
 
 def guardar_pedido(nombre, apellidos):
     """ Función Guarda pedido """
-    with open("pedidos.txt", "a",  encoding="utf-8",) as file:
-        file.write("-" + nombre + " " + apellidos + "\n")
-        file.close()
+    with open("pedidos.txt", "a",  encoding="utf-8",) as file_3:
+        file_3.write("-" + nombre + " " + apellidos + "\n")
+        file_3.close()
