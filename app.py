0ffc735da0d772d51e7718081208208954e1a6c7
Alander Gimenez <82914806+alandergimenez@users.noreply.github.com>
2023-01-07 15:44:30 +0100

    App finalizada con retorno de msg al front

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """ Mi App en Python """
 
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 import persistencia
 
 app = Flask(__name__)
@@ -14,4 +14,26 @@ def pizza():
     print(nombre)
     print(apellidos)
     return redirect("http://localhost/html/solicita_pedido.html", code=302)
+
+
+@app.route("/checksize", methods=['POST']) 
+def checksize():  
+    """  Comprueba disponibilidad de un tamaño de pizza.  """     
+
+    tamano  = request.form.get("tamano")
+
+    print(tamano)
+
+    if tamano == "S":
+        mensaje = "No disponsible"
+        return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'}) 
+
+    elif tamano == "N":
+        mensaje = ""
+        return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'}) 
+
+    else:
+        mensaje = "Disponsible"
+        return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'}) 
+
 app.run()
